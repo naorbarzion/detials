447c570bde411699882c3e1f7e3055727ea8467a
naorbarzion <149252537+naorbarzion@users.noreply.github.com>
2024-12-21 06:57:05 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -46,7 +46,7 @@ async function submitForm(event) {
 
   try {
     const response = await fetch(
-      'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwv2FcJ33HNlOW6vj3_oWnpM1ENAD80qPsOM-zxq8-WckJiokf6scG1MQeymwpzwrp_SQ/exec/exec',
+      'https://script.google.com/macros/s/https://script.google.com/macros/s/AKfycbwv2FcJ33HNlOW6vj3_oWnpM1ENAD80qPsOM-zxq8-WckJiokf6scG1MQeymwpzwrp_SQ/exec',
       {
         method: 'POST',
         headers: {
@@ -74,5 +74,3 @@ async function submitForm(event) {
 }
 
 document.getElementById('contactForm').addEventListener('submit', submitForm);
-
-
